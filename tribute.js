d4de65592c9e487e25690e5bd3f695d60afceaa2
Mohan-Marnattu <105000988+Mohan-Marnattu@users.noreply.github.com>
2022-05-05 08:23:01 -0700

    Add files via upload

---
+++ tribute.js
@@ -0,0 +1,45 @@
+body {
+    background-color: #222423;
+    margin: 20%;
+    color: antiquewhite;
+}
+
+#title {
+    text-align: center;
+    text-shadow: 5px 5px 10px white;
+    font-size: 7vh;
+}
+img {
+    
+    width: 750px;
+    
+}
+
+#caption {
+    font-size: 27px;
+    font-family: Gill Sans;
+    color: antiquewhite;
+}
+
+div#tribute-data {
+    background-color: rgba(51, 56, 53, 0.25);
+    box-shadow: 20px 20px 20px #93c593;
+    font-family: Georgia;
+    padding: 25px 25px;
+    margin: 11px;
+    margin-top: 50px;
+}
+
+
+h1.title-APJ {
+    font-size: 35px;
+    color: white;
+    text-align: center;
+    text-shadow: 5px 5px 10px black;
+}
+
+
+#tribute-link {
+    text-decoration: none;
+    color: black;
+}
